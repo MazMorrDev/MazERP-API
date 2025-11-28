e08487ceb4517adcb4708a129bfcc96e69af9499
MazMorrDev <marconchelo12@gmail.com>
2025-11-28 13:14:30 -0500

    feat: add client model

--- Models/Client.cs
+++ Models/Client.cs
@@ -2,5 +2,10 @@
 
 public class Client
 {
-
+    public int Id;
+    public string? Name;
+    public string? Password_Hash;
+    public string? profile_photo_url;
+    public string? email;
+    public DateTime paid_license_date;
 }
