723df191cc27ef5853533eaec67c7ba74fa68c72
MazMorrDev <marconchelo12@gmail.com>
2025-11-28 14:06:02 -0500

    ref: add column names and others annotations

--- Models/Client.cs
+++ Models/Client.cs
@@ -1,6 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using Microsoft.EntityFrameworkCore;
 
 namespace MazErpBack;
 
@@ -9,33 +8,45 @@ public class Client
 {
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Column("id")]
     public int Id { get; set; }
 
     // Decidí hacer que el email no fuese un PK porq una persona puede perder su email
     // habría que hacer un sistema para cambiar su email
+    // TODO: al email hay que hacerle un índice único en la configuración del context para evitar duplicados
     [Required]
     [EmailAddress]
     [MaxLength(255)]
+    [Column("email")]
     public string Email { get; set; } = string.Empty;
 
     [Required]
     [MaxLength(100)]
+    [Column("name")]
     public string Name { get; set; } = string.Empty;
 
     [Required]
     [MaxLength(255)]
+    [Column("password_hash")]
     public string PasswordHash { get; set; } = string.Empty;
 
-    // Lógica de licencia mejorada
+    // Lógica de licencia
+    [Column("license_start_date")]
     public DateTime LicenseStartDate { get; set; } = DateTime.UtcNow;
+
+    // Lógica para saber si el cliente aún tiene la licensia activa
+    // public bool IsLicenseActive => DateTime.UtcNow <= LicenseEndDate;
+
+    [Column("license_end_date")]
     public DateTime LicenseEndDate { get; set; } = DateTime.UtcNow.AddMonths(1);
-    
-    public bool IsLicenseActive => DateTime.UtcNow <= LicenseEndDate;
 
     [MaxLength(500)]
+    [Column("profile_photo_url")]
     public string? ProfilePhotoUrl { get; set; }
 
     // Auditoría
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    [Column("created_at")]
+    public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
+    [Column("updated_at")]
+    public DateTimeOffset UpdatedAt { get; set; } = DateTimeOffset.UtcNow;
 }
