9729e75ba70a0dc99d0df4cd46e271337d8f91ea
MazMorrDev <marconchelo12@gmail.com>
2025-11-29 15:45:22 -0500

    feat: add workflow model

---
+++ Models/Workflow.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MazErpBack;
+
+[Table("workflow")]
+public class Workflow
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Column("id")]
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(30)]
+    [Column("name")]
+    public string Name { get; set; } = string.Empty;
+
+    [MaxLength(255)]
+    [Column("description")]
+    public string? Description { get; set; }
+
+    [MaxLength(500)]
+    [Column("workflow_photo_url")]
+    public string? WorkflowPhotoUrl { get; set; }
+
+    [Column("created_at")]
+    public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
+}
