e73f0f7ec2d273b905e26e21ed95b778e09e65b3
MazMorrDev <marconchelo12@gmail.com>
2025-11-30 02:04:30 -0500

    fix: column name at roles property

--- Models/Inventory.cs
+++ Models/Inventory.cs
@@ -1,6 +1,28 @@
-﻿namespace MazErpBack;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace MazErpBack;
+
+[Table("Inventory")]
 public class Inventory
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Column("id")]
+    public int Id { get; set; }
 
+    [Required]
+    [Column("warehouse_id")]
+    public int WarehouseId { get; set; }
+
+    [Required]
+    [Column("product_id")]
+    public int ProductId { get; set; }
+
+    // Navigation Properties
+    [ForeignKey("WarehouseId")]
+    public virtual Warehouse Warehouse { get; set; } = null!;
+
+    [ForeignKey("ProductId")]
+    public virtual Product Product { get; set; } = null!;
 }
