5ee4132418056b596a1f46c3007aaef27b470c4c
MazMorrDev <marconchelo12@gmail.com>
2025-11-30 01:24:04 -0500

    feat: add warehouse model

--- Models/Warehouse.cs
+++ Models/Warehouse.cs
@@ -1,6 +1,30 @@
-﻿namespace MazErpBack;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace MazErpBack;
+
+[Table("Warehouse")]
 public class Warehouse
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Column("id")]
+    public int Id { get; set; }
 
+    // Foreign Key para Workflow
+    [Column("workflow_id")]
+    public int? WorkflowId { get; set; }
+
+    [Required]
+    [MaxLength(30)]
+    [Column("name")]
+    public string Name { get; set; } = string.Empty;
+
+    [MaxLength(255)]
+    [Column("description")]
+    public string? Description { get; set; }
+
+    // Navigation property
+    [ForeignKey("WorkflowId")]
+    public virtual Workflow? Workflow { get; set; }
 }
