8d674e86a817e660df9f546a6ca77ef01e3c4b3e
MazMorrDev <marconchelo12@gmail.com>
2025-11-30 02:31:57 -0500

    feat: add Movement model

--- Models/Movement.cs
+++ Models/Movement.cs
@@ -10,4 +10,47 @@ public class Movement
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     [Column("id")]
     public int Id { get; set; }
+
+    [Required]
+    [Column("client_id")]
+    public int ClientId { get; set; }
+
+    [Required]
+    [Column("warehouse_id")]
+    public int WarehouseId { get; set; }
+
+    [Required]
+    [Column("product_id")]
+    public int ProductId { get; set; }
+
+    [MaxLength(225)]
+    [Column("description")]
+    public string? Description { get; set; }
+
+    [Required]
+    [Column("movement_type")]
+    public MovementType MovementType { get; set; }
+
+    [Required]
+    [Column("quantity")]
+    public int Quantity { get; set; }
+
+    [Required]
+    [Column("unitary_cost")]
+    public double UnitaryCost { get; set; }
+
+    [Required]
+    [Column("movement_date")]
+    public DateTime MovementDate { get; set; }
+
+    // Navigation Properties
+    [ForeignKey("ClientId")]
+    public virtual Client Client { get; set; } = null!;
+
+    [ForeignKey("WarehouseId")]
+    public virtual Warehouse Warehouse { get; set; } = null!;
+
+    [ForeignKey("ProductId")]
+    public virtual Product Product { get; set; } = null!;
+
 }
