567d012ef1d8fd5dc749686a78153c86a240d605
MazMorrDev <marconchelo12@gmail.com>
2025-11-30 18:31:29 -0500

    feat: add all models to context

--- Context/AppDbContext.cs
+++ Context/AppDbContext.cs
@@ -1,6 +1,21 @@
-﻿namespace MazErpBack;
+﻿using Microsoft.EntityFrameworkCore;
 
-public class AppDbContext
+namespace MazErpBack;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
+    public DbSet<Client> Clients { get; set; }
+    public DbSet<ClientWorkflow> ClientWorkflows { get; set; }
+    public DbSet<Inventory> Inventories { get; set; }
+    public DbSet<Movement> Movements { get; set; }
+    public DbSet<Product> Products{get; set;}
+    public DbSet<Warehouse> Warehouses {get; set;}
+    public DbSet<Workflow> Workflows {get; set;}
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Todo: Configurar esto acá
+    }
 }
