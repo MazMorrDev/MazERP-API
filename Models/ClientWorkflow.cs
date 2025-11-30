1deb73cec6241f54d272bb3d1c95f62adced5d81
MazMorrDev <marconchelo12@gmail.com>
2025-11-30 01:46:14 -0500

    feat: add ClientWorkflow model

--- Models/ClientWorkflow.cs
+++ Models/ClientWorkflow.cs
@@ -1,6 +1,31 @@
-﻿namespace MazErpBack;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace MazErpBack;
+
+[Table("ClientWorkflows")]
 public class ClientWorkflow
 {
+    // Clave primaria compuesta - Parte 1
+    [Column("client_id")]
+    public int ClientId { get; set; }
 
+    // Clave primaria compuesta - Parte 2
+    [Column("workflow_id")]
+    public int WorkflowId { get; set; }
+
+    [MaxLength(500)]
+    [Column("notes")]
+    public ClientWorkflowRole Role { get; set; }
+
+    [Column("assigned_at")]
+    public DateTimeOffset AssignedAt { get; set; } = DateTimeOffset.UtcNow;
+
+
+    // Navigation properties
+    [ForeignKey("ClientId")]
+    public virtual Client Client { get; set; } = null!;
+
+    [ForeignKey("WorkflowId")]
+    public virtual Workflow Workflow { get; set; } = null!;
 }
