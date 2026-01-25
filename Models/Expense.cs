0a1ae6018172b63e9c8a489b669916262552cf31
Marco Antonio Romero Albanez <122071151+MazMorrDev@users.noreply.github.com>
2026-01-24 22:36:28 -0500

    feat: add Expense entity

---
+++ Models/Expense.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MazErpBack.Models;
+
+public class Expense
+{
+    [Key, Column("id")]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+}
