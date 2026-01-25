f767e9b89a7b9b272cdd33d85cd569baac7054e8
Marco Antonio Romero Albanez <122071151+MazMorrDev@users.noreply.github.com>
2026-01-24 22:48:45 -0500

    feat: add enums for expense entity

--- Models/Expense.cs
+++ Models/Expense.cs
@@ -8,4 +8,42 @@ public class Expense
     [Key, Column("id")]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
+
+    [Required, Column("user_id")]
+    public int UserId { get; set; }
+
+    [Required, Column("inventory_id")]
+    public int InventoryId { get; set; }
+
+    [Column("description")]
+    public string? Description { get; set; }
+
+    [Required, Column("category")]
+    public required ExpenseCategory Category { get; set; }
+
+    [Column("amount")]
+    public int Amount { get; set; }
+
+    [Column("payment_method")]
+    public PaymentMethod PaymentMethod { get; set; }
+
+    // Auditoría
+    [Column("date_paid")]
+    public DateTimeOffset DatePaid { get; set; }
+
+    [Column("created_at")]
+    public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
+
+    [Column("updated_at")]
+    public DateTimeOffset UpdatedAt { get; set; } = DateTimeOffset.UtcNow;
+
+    [Column("is_active")]
+    public bool IsActive { get; set; } = true;
+
+    // Navigation Properties
+    [ForeignKey("UserId")]
+    public virtual User User { get; set; } = null!;
+
+    [ForeignKey("InventoryId")]
+    public virtual Inventory Inventory { get; set; } = null!;
 }
