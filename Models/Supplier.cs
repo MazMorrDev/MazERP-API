44787492e5a870658aa9c1f6259ce5f1caea5bfc
Marco Antonio Romero Albanez <122071151+MazMorrDev@users.noreply.github.com>
2026-01-24 22:24:53 -0500

    feat: add supplier

---
+++ Models/Supplier.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MazErpBack.Models;
+
+[Table("Supplier")]
+public class Supplier
+{
+    [Key, Column("id")]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    [Column("name")]
+    public string Name { get; set; }
+
+    [Column("contact_person")]
+    public string? ContactPerson {get; set;}
+}
