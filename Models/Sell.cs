4111f61795d6bdd6804ce51c5d50cbf7f7623ee9
Marco Antonio Romero Albanez <122071151+MazMorrDev@users.noreply.github.com>
2026-01-27 11:52:23 -0500

    feat(Sell): add all designed attributes

--- Models/Sell.cs
+++ Models/Sell.cs
@@ -10,9 +10,19 @@ public class Sell
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
 
-    [Required, Column("name")]
-    public required string Name { get; set; }
+    [Required, Column("invoice_number")]
+    public required int InvoiceNumber { get; set; }
 
-    [Column("contact_person")]
-    public string? ContactPerson { get; set; }
+    [Column("discount_percentage", TypeName = "decimal(5,2)")]
+    [Range(0, 100, ErrorMessage = "El porcentage de descuento no puede ser menor a 0 ni mayor a 100")]
+    public decimal DiscountPercentage { get; set; }
+
+    [Column("payment_status")]
+    public PaymentStatus PaymentStatus { get; set; }
+
+    [Column("sale_type")]
+    public SaleType SaleType { get; set; }
+
+    [Column("seller_notes")]
+    public string? SellerNotes { get; set; }
 }
