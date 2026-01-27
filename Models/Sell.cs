01b18b65007b8dae16819460fa18a30afcacfe35
Marco Antonio Romero Albanez <122071151+MazMorrDev@users.noreply.github.com>
2026-01-27 09:43:29 -0500

    feat(Sell): add initial attributes

--- Models/Sell.cs
+++ Models/Sell.cs
@@ -1,6 +1,18 @@
-﻿namespace MazErpBack;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace MazErpBack.Models;
+
+[Table("Sell")]
 public class Sell
 {
+    [Key, Column("id")]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
 
+    [Required, Column("name")]
+    public required string Name { get; set; }
+
+    [Column("contact_person")]
+    public string? ContactPerson { get; set; }
 }
