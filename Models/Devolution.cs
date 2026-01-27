f18945805e3c400965b525938ab343e2dca81ee2
Marco Antonio Romero Albanez <122071151+MazMorrDev@users.noreply.github.com>
2026-01-27 14:02:46 -0500

    feat(Devolution): add Sell FK

--- Models/Devolution.cs
+++ Models/Devolution.cs
@@ -1,6 +1,20 @@
-﻿namespace MazErpBack.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MazErpBack.Models;
 
 public class Devolution
 {
+    [Key, Column("id")]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
 
+    [Column("sell_id")]
+    public required int SellId { get; set; }
+
+    
+
+    // Navigation Properties
+    [ForeignKey("SellId")]
+    public required virtual Sell Sell { get; set; }
 }
