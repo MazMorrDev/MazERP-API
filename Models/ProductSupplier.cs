3ab454db9fc87f1e1e2e15679c3ee939004e519f
Marco Antonio Romero Albanez <122071151+MazMorrDev@users.noreply.github.com>
2026-01-27 19:32:45 -0500

    feat(Devolution): add ProductSupplier initial attributes

--- Models/ProductSupplier.cs
+++ Models/ProductSupplier.cs
@@ -1,6 +1,23 @@
-﻿namespace MazErpBack.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace MazErpBack.Models;
+
+[Table("Product_Supplier")]
 public class ProductSupplier
 {
+    [Required, Column("product_id")]
+    public required int ProductId { get; set; }
 
+    [Required, Column("supplier_id")]
+    public required int SupplierId { get; set; }
+
+    
+
+    // Navigation Properties
+    [ForeignKey("ProductId")]
+    public required virtual Product Product { get; set; }
+
+    [ForeignKey("SupplierId")]
+    public required virtual Supplier Supplier { get; set; }
 }
