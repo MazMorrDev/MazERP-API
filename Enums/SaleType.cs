c9a78002445cf63b4d21671aa6556a45cdc1f534
Marco Antonio Romero Albanez <122071151+MazMorrDev@users.noreply.github.com>
2026-01-28 22:08:07 -0500

    feat(SaleTypeEnum): add attributes

--- Enums/SaleType.cs
+++ Enums/SaleType.cs
@@ -2,5 +2,13 @@
 
 public enum SaleType
 {
-
+    Retail,         // Venta al por menor
+    Wholesale,      // Venta al por mayor
+    Online,         // Venta en línea
+    InStore,        // Venta en tienda
+    Subscription,   // Suscripción
+    Service,        // Servicio
+    Contract,       // Contrato
+    Bid,            // Licitación
+    Other           // Otro
 }
