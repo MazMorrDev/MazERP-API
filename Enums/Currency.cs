a38cdf3b6782a0398b9d3519ea149831f7283fed
Marco Antonio Romero Albanez <122071151+MazMorrDev@users.noreply.github.com>
2026-01-28 21:59:07 -0500

    ref(CurrencyEnum): add standard currencies

--- Enums/Currency.cs
+++ Enums/Currency.cs
@@ -2,5 +2,15 @@
 
 public enum Currency
 {
-
+    USD,  // Dólar estadounidense
+    EUR,  // Euro
+    GBP,  // Libra esterlina
+    JPY,  // Yen japonés
+    CNY,  // Yuan chino
+    MXN,  // Peso mexicano
+    PEN,  // Sol peruano
+    COP,  // Peso colombiano
+    ARS,  // Peso argentino
+    BRL,  // Real brasileño
+    CLP   // Peso chileno
 }
