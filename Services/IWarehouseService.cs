07d965c14fd71a91a7a18428a77f02f3db82f6e0
Marco Antonio Romero Albanez <122071151+MazMorrDev@users.noreply.github.com>
2026-02-01 17:13:05 -0500

    feat(IWarehouseService): add methods

--- Services/IWarehouseService.cs
+++ Services/IWarehouseService.cs
@@ -6,9 +6,13 @@ namespace MazErpBack.Services;
 public interface IWarehouseService
 {
     // For admin pannel or back operations
+    public Task<List<Warehouse>> GetWarehousesAsync();
+    public Task<Warehouse> GetWarehouseByIdAsync(int warehouseId);
     public Task DeleteWarehouseAsync(int id);
 
     // For common users
-    public Task<Warehouse> CreateWarehouseAsync(CreateWarehouseDto warehouseDto);
     public Task<List<Warehouse>> GetWarehousesByWorkflowAsync(int workflowId);
+    public Task<Warehouse> CreateWarehouseAsync(CreateWarehouseDto warehouseDto);
+    public Task<Warehouse> UpdateWarehouseAsync(UpdateWarehouseDto warehouseDto);
+    public Task<Warehouse> SoftDeleteWarehouseAsync(int warehouseId);
 }
