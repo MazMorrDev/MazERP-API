97649bcbc48fa42a49247c86e301f0bec2e3ea27
Marco Antonio Romero Albanez <122071151+MazMorrDev@users.noreply.github.com>
2026-01-31 21:54:49 -0500

    feat(ISellService): add methods

--- Services/ISellService.cs
+++ Services/ISellService.cs
@@ -1,6 +1,18 @@
-﻿namespace MazErpBack.Services;
+﻿using MazErpBack.Models;
+
+namespace MazErpBack.Services;
 
 public interface ISellService
 {
+    // Only avaible for admin pannel or backend operations
+    public Task<List<Sell>> GetSellsAsync();
+    public Task<Sell> GetSellById(int sellId);
+    public Task<Sell> DeleteSellAsync(int sellId);
 
+    // For common users
+    public Task<List<Sell>> GetSellsByWorkflowAsync(int workflowId);
+    public Task<List<Sell>> GetSellsByWarehouseAsync(int warehouseId);
+    public Task<Sell> CreateSellAsync(CreateSellDto sellDto);
+    public Task<Sell> UpdateSellAsync(UpdateSellDto sellDto);
+    public Task<Sell> SoftDeleteSellAsync(int sellID);
 }
