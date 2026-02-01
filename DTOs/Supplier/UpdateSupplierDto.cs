92137959972652e58661a31036b93c82e7a3effc
Marco Antonio Romero Albanez <122071151+MazMorrDev@users.noreply.github.com>
2026-02-01 12:15:43 -0500

    ref: rorganize Dtos namespaces

--- DTOs/Supplier/UpdateSupplierDto.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace MazErpBack.DTOs.Supplier;
-
-public record class UpdateSupplierDto
-{
-
-}
--- DTOs/Users/DeleteUserWorkflowDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MazErpBack.DTOs.Users;
-
-public record class DeleteUserWorkflowDto
-{
-    public int UserId { get; init; }
-    public int WorkflowId { get; init; }
-}
