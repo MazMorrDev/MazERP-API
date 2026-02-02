faf23951f2dbf0fd40b959b9c264cc60f5ae2ea8
Marco Antonio Romero Albanez <122071151+MazMorrDev@users.noreply.github.com>
2026-02-02 15:21:13 -0500

    feat(WarehouseDto): add attributes

--- DTOs/Inventory/WarehouseDto.cs
+++ DTOs/Inventory/WarehouseDto.cs
@@ -2,5 +2,8 @@
 
 public record class WarehouseDto
 {
-
+    public int WarehouseId { get; init; }
+    public int WorkflowId { get; init; }
+    public required string Name { get; init; }
+    public string? Description { get; init; }
 }
