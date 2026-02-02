72a704892fc1c5d056efcbe47d741d68bfaca337
Marco Antonio Romero Albanez <122071151+MazMorrDev@users.noreply.github.com>
2026-02-02 14:51:10 -0500

    feat(SupplierDto): add attributes

--- DTOs/Inventory/CreateSupplierDto.cs
+++ DTOs/Inventory/CreateSupplierDto.cs
@@ -1,6 +1,14 @@
-﻿namespace MazErpBack.DTOs.Inventory;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MazErpBack.DTOs.Inventory;
 
 public record class CreateSupplierDto
 {
-
+    public required string Name { get; init; }
+    [EmailAddress]
+    public string? Email { get; init; }
+    [Phone]
+    public string? PhoneNumber { get; init; }
+    public string? Location { get; init; }
+    public int? Rating { get; init; }
 }
--- DTOs/Inventory/SupplierDto.cs
+++ DTOs/Inventory/SupplierDto.cs
@@ -1,6 +1,16 @@
-﻿namespace MazErpBack.DTOs.Inventory;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MazErpBack.DTOs.Inventory;
 
 public record class SupplierDto
 {
-    
+    public required int SupplierId { get; init; }
+    public required string Name { get; init; }
+    [EmailAddress]
+    public string? Email { get; init; }
+    [Phone]
+    public string? PhoneNumber { get; init; }
+    public string? Location { get; init; }
+    public int? Rating { get; init; }
 }
