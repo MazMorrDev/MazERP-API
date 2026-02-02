696c5d87e6cc8681d1f436802ff0c37364070804
Marco Antonio Romero Albanez <122071151+MazMorrDev@users.noreply.github.com>
2026-02-02 14:09:07 -0500

    feat(SellDto): add attributes

--- DTOs/Movements/SellDto.cs
+++ DTOs/Movements/SellDto.cs
@@ -1,6 +1,11 @@
-﻿namespace MazErpBack.DTOs.Movements;
+﻿using MazErpBack.Enums;
+
+namespace MazErpBack.DTOs.Movements;
 
 public record class SellDto : MovementDto
 {
-
+    public decimal DiscountPercentage { get; init; }
+    public PaymentStatus PaymentStatus { get; init; }
+    public SaleType SaleType { get; init; }
+    public string? SellerNotes { get; init; }
 }
--- DTOs/Movements/SellOperationDto.cs
+++ DTOs/Movements/SellOperationDto.cs
@@ -1,6 +1,11 @@
-﻿namespace MazErpBack.DTOs.Movements;
+﻿using MazErpBack.Enums;
 
-public record class SellOperationDto
+namespace MazErpBack.DTOs.Movements;
+
+public record class SellOperationDto : MovementOperationDto
 {
-    
+    public decimal DiscountPercentage { get; init; }
+    public PaymentStatus PaymentStatus { get; init; }
+    public SaleType SaleType { get; init; }
+    public string? SellerNotes { get; init; }
 }
