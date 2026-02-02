b3f642ed84f53cac89b2295657a5265c67c329b5
Marco Antonio Romero Albanez <122071151+MazMorrDev@users.noreply.github.com>
2026-02-02 14:13:58 -0500

    feat(ExpenseDto): add attributes

--- DTOs/Movements/ExpenseDto.cs
+++ DTOs/Movements/ExpenseDto.cs
@@ -1,6 +1,15 @@
-﻿namespace MazErpBack.DTOs.Movements;
+﻿using MazErpBack.Enums;
+
+namespace MazErpBack.DTOs.Movements;
 
 public record class ExpenseDto
 {
-
+    public required int ExpenseId { get; init; }
+    public required int UserId { get; init; }
+    public required int InventoryId { get; init; }
+    public string? Description { get; init; }
+    public ExpenseCategory ExpenseCategory { get; init; }
+    public decimal Amount { get; init; }
+    public PaymentMethod PaymentMethod { get; init; }
+    public DateTimeOffset DatePaid { get; init; }
 }
--- DTOs/Movements/ExpenseOperationDto.cs
+++ DTOs/Movements/ExpenseOperationDto.cs
@@ -1,6 +1,14 @@
-﻿namespace MazErpBack.DTOs.Movements;
+﻿using MazErpBack.Enums;
+
+namespace MazErpBack.DTOs.Movements;
 
 public record class ExpenseOperationDto
 {
-
+    public required int UserId { get; init; }
+    public required int InventoryId { get; init; }
+    public string? Description { get; init; }
+    public ExpenseCategory ExpenseCategory { get; init; }
+    public decimal Amount { get; init; }
+    public PaymentMethod PaymentMethod { get; init; }
+    public DateTimeOffset DatePaid { get; init; }
 }
