50ef77b7382b4b14733525600452ec57742aa97a
Marco Antonio Romero Albanez <122071151+MazMorrDev@users.noreply.github.com>
2026-02-03 20:45:32 -0500

    feat(DeleteUserWorkflowDto): add [Required] annotation to IDs

--- DTOs/Workflow/DeleteUserWorkflowDto.cs
+++ DTOs/Workflow/DeleteUserWorkflowDto.cs
@@ -1,7 +1,12 @@
-﻿namespace MazErpBack.DTOs.Workflow;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MazErpBack.DTOs.Workflow;
 
 public record class DeleteUserWorkflowDto
 {
+    [Required]
     public int UserId { get; init; }
+    
+    [Required]
     public int WorkflowId { get; init; }
 }
