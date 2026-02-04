cbae935ace369de97329d93629a8a68a3b29bdb5
Marco Antonio Romero Albanez <122071151+MazMorrDev@users.noreply.github.com>
2026-02-03 21:11:25 -0500

    ref(IProductService): use Dto instead of the model

--- Services/IProductService.cs
+++ Services/IProductService.cs
@@ -6,13 +6,13 @@ namespace MazErpBack.Services;
 public interface IProductService
 {
     // Only avaible for admin pannel or backend operations
-    public Task<List<Product>> GetProductsAsync();
-    public Task<Product> GetProductByIdAsync(int productId);
-    public Task DeleteProductAsync(int productId);
+    public Task<List<ProductDto>> GetProductsAsync();
+    public Task<ProductDto> GetProductByIdAsync(int productId);
+    public Task<bool> DeleteProductAsync(int productId);
 
     // For common users
-    public Task<List<Product>> GetProductsByWorkflowAsync(int workflowId);
-    public Task<List<Product>> GetProductsByWarehouseAsync(int warehouseId);
-    public Task<Product> CreateProductAsync(CreateProductDto productDto);
-    public Task<Product> UpdateProductAsync(CreateProductDto productDto);
+    public Task<List<ProductDto>> GetProductsByWorkflowAsync(int workflowId);
+    public Task<List<ProductDto>> GetProductsByWarehouseAsync(int warehouseId);
+    public Task<ProductDto> CreateProductAsync(CreateProductDto productDto);
+    public Task<ProductDto> UpdateProductAsync(CreateProductDto productDto);
 }
