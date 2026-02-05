416f4a6cc8976a657e49bf966eb19e5d36cc353d
Marco Antonio Romero Albanez <122071151+MazMorrDev@users.noreply.github.com>
2026-02-05 13:37:02 -0500

    feat: add DeleteSellPointInventoryDto

--- Services/ISellPointInventoryService.cs
+++ Services/ISellPointInventoryService.cs
@@ -1,6 +1,13 @@
-﻿namespace MazErpBack.Services;
+﻿using MazErpBack.DTOs.Inventory;
+
+namespace MazErpBack.Services;
 
 public interface ISellPointInventoryService
 {
+    // This method has to ensure that if an user is already at the wf then update the values
+    public Task<SellPointInventoryDto> AssignUserWithRoleToCompanyAsync(SellPointInventoryDto sellPointInventoryDto);
+    public Task<bool> SoftDeleteUserCompanyAsync(DeleteSellPointInventoryDto sellPointInventoryDto);
 
+    // For very old registries that we don't want
+    public Task<bool> DeleteUserCompanyAsync(DeleteSellPointInventoryDto sellPointInventoryDto);
 }
