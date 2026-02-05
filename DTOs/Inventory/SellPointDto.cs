4a80b1cab0836aad12af98f97487d2f720837d54
Marco Antonio Romero Albanez <122071151+MazMorrDev@users.noreply.github.com>
2026-02-05 12:58:19 -0500

    feat: add SellPointDto attributes

--- DTOs/Inventory/SellPointDto.cs
+++ DTOs/Inventory/SellPointDto.cs
@@ -1,6 +1,8 @@
-﻿namespace MazErpBack;
+﻿namespace MazErpBack.DTOs.Inventory;
 
 public record class SellPointDto
 {
-
+    public required int Id { get; init; }
+    public required string Name { get; init; }
+    public string? Location { get; init; }
 }
