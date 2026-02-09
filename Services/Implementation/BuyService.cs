de9c55b183e924ace9a3633a2eeb383ef902800d
Marco Antonio Romero Albanez <122071151+MazMorrDev@users.noreply.github.com>
2026-02-08 20:04:21 -0500

    feat(BuyService): implement CreateBuyAsync method

--- Services/Implementation/BuyService.cs
+++ Services/Implementation/BuyService.cs
@@ -1,47 +1,64 @@
-﻿using MazErpBack.DTOs.Movements;
+﻿using MazErpBack.Context;
+using MazErpBack.DTOs.Movements;
 using MazErpBack.Models;
 using MazErpBack.Services.Interfaces;
+using MazErpBack.Utils.Mappers;
 
 namespace MazErpBack.Services.Implementation;
 
-public class BuyService : IBuyService
+public class BuyService(AppDbContext context, BuyMapper mapper) : IBuyService
 {
-    public Task<BuyDto> CreateBuyAsync(CreateBuyDto buyDto)
+    private readonly AppDbContext _context = context;
+    private readonly BuyMapper _mapper = mapper;
+    public async Task<BuyDto> CreateBuyAsync(CreateBuyDto createBuyDto)
+    {
+        var supplier = await _context.Suppliers.FindAsync(createBuyDto.SupplierId);
+        var user = await _context.Users.FindAsync(createBuyDto.UserId);
+        var sellPoint = await _context.SellPoints.FindAsync(createBuyDto.SellPointId);
+        if (supplier == null || user == null || sellPoint == null) throw new ArgumentNullException();
+
+        var movement = _mapper.MapMovement(user, sellPoint, createBuyDto);
+        var buy = _mapper.MapBuy(movement, supplier, createBuyDto);
+        await _context.Movements.AddAsync(movement);
+        await _context.Buys.AddAsync(buy);
+        await _context.SaveChangesAsync();
+
+        var buyDto = _mapper.MapToDto(movement, buy);
+        ArgumentNullException.ThrowIfNull(buyDto);
+        return buyDto;
+    }
+
+    public async Task<bool> DeleteBuyAsync(int buyId)
     {
         throw new NotImplementedException();
     }
 
-    public Task<bool> DeleteBuyAsync(int buyId)
+    public async Task<Buy> GetBuyByIdAsync(int buyId)
     {
         throw new NotImplementedException();
     }
 
-    public Task<Buy> GetBuyByIdAsync(int buyId)
+    public async Task<List<Buy>> GetBuysAsync()
     {
         throw new NotImplementedException();
     }
 
-    public Task<List<Buy>> GetBuysAsync()
+    public async Task<List<BuyDto>> GetBuysByWarehouseAsync(int warehouseId)
     {
         throw new NotImplementedException();
     }
 
-    public Task<List<BuyDto>> GetBuysByWarehouseAsync(int warehouseId)
+    public async Task<List<BuyDto>> GetBuysByWorkflowAsync(int workflowId)
     {
         throw new NotImplementedException();
     }
 
-    public Task<List<BuyDto>> GetBuysByWorkflowAsync(int workflowId)
+    public async Task<bool> SoftDeleteBuyAsync(int buyID)
     {
         throw new NotImplementedException();
     }
 
-    public Task<bool> SoftDeleteBuyAsync(int buyID)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<BuyDto> UpdateBuyAsync(int buyId, CreateBuyDto buyDto)
+    public async Task<BuyDto> UpdateBuyAsync(int buyId, CreateBuyDto buyDto)
     {
         throw new NotImplementedException();
     }
--- Utils/Mappers/BuyMapper.cs
+++ Utils/Mappers/BuyMapper.cs
@@ -32,6 +32,36 @@ public class BuyMapper(ILogger<BuyMapper> logger)
         };
     }
 
+    public Buy MapBuy(Movement movement, Supplier supplier, CreateBuyDto buyDto)
+    {
+        return new Buy
+        {
+            MovementId = movement.Id,
+            SupplierId = buyDto.SupplierId,
+            DeliveryStatus = buyDto.DeliveryStatus,
+            UnitaryCost = buyDto.UnitaryCost,
+            ReceivedQuantity = buyDto.ReceivedQuantity,
+            Movement = movement,
+            Supplier = supplier
+        };
+    }
+
+    public Movement MapMovement(User user, SellPoint sellPoint, CreateBuyDto buyDto)
+    {
+        return new Movement
+        {
+            UserId = buyDto.UserId,
+            SellPointId = buyDto.SellPointId,
+            Description = buyDto.Description,
+            MovementType = buyDto.MovementType,
+            Quantity = buyDto.Quantity,
+            Currency = buyDto.Currency,
+            MovementDate = buyDto.MovementDate,
+            SellPoint = sellPoint,
+            User = user
+        };
+    }
+
     public List<BuyDto>? MapListToDto(List<Movement> movements, List<Buy> buys)
     {
         List<BuyDto> buysDto = [];
