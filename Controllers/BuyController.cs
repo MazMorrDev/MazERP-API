97849bb103badc992280fca938345acc103a49b2
Marco Antonio Romero Albanez <122071151+MazMorrDev@users.noreply.github.com>
2026-03-03 19:27:31 -0500

    feat: add BuyController

---
+++ Controllers/BuyController.cs
@@ -0,0 +1,16 @@
+using MazErpBack.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MazErpBack.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+[Authorize]
+public class BuyController(IBuyService service, ILogger<BuyController> logger) : ControllerBase
+{
+    public IBuyService _service = service;
+    public ILogger<BuyController> _logger = logger;
+
+    
+}
