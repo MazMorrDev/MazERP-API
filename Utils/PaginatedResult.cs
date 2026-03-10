dc189002801c538a52c15c7eb8fe72f8195fa00c
Marco Antonio Romero Albanez <122071151+MazMorrDev@users.noreply.github.com>
2026-03-10 09:32:21 -0400

    feat: add PaginatedResult record for pagination use

---
+++ Utils/PaginatedResult.cs
@@ -0,0 +1,8 @@
+namespace MazErpBack.Utils;
+
+public record PaginatedResult<T>(IEnumerable<T> Items, int TotalCount, int PageNumber, int PageSize)
+{
+    public int TotalPages => (int)Math.Ceiling(TotalCount / (double)PageSize);
+    public bool HasPreviousPage => PageNumber > 1;
+    public bool HasNextPage => PageNumber < TotalPages;
+}
