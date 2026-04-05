ec05d7ce72c56aca125df4c11579661579b21eb3
Marco Antonio Romero Albanez <122071151+MazMorrDev@users.noreply.github.com>
2026-04-04 23:35:53 -0400

    test: add my first unit test to the project :D

--- UnitTests/UserServiceTest.cs
+++ UnitTests/UserServiceTest.cs
@@ -1,8 +1,11 @@
 ﻿using MazErpAPI.Context;
 using MazErpAPI.Controllers;
+using MazErpAPI.DTOs.Users;
+using MazErpAPI.Models;
 using MazErpAPI.Services.Implementation;
 using MazErpAPI.Services.Interfaces;
 using MazErpAPI.Utils.Mappers;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
@@ -12,17 +15,14 @@ namespace UnitTests;
 
 public class UserServiceTest
 {
-    private readonly Mock<IConfiguration> _mockConfig;
     private readonly Mock<ILogger<UserService>> _mockLogger;
     private readonly Mock<ITokenService> _mockTokenService;
     private readonly UserMapper _mapper;
     private readonly AppDbContext _context;
     private readonly UserService _userService;
-    private readonly UserController _controller;
 
     public UserServiceTest()
     {
-        _mockConfig = new Mock<IConfiguration>();
         _mockLogger = new Mock<ILogger<UserService>>();
         _mockTokenService = new Mock<ITokenService>();
         _mapper = new UserMapper();
@@ -34,20 +34,37 @@ public class UserServiceTest
 
         _context = new AppDbContext(options);
         _userService = new UserService(_context, _mockTokenService.Object, _mockLogger.Object, _mapper);
-        _controller = new UserController(_userService);
     }
 
 
     [Fact]
-    public void LoginUser_WithInvalidPassword_ReturnsNull()
+    public async Task LoginUser_WithInvalidPassword_ReturnsArgumentException()
     {
         // Arrange
+        var user = new User
+        {
+            Id = 1,
+            Email = "test@example.com",
+            Name = "testuser",
+            LicenseStartDate = DateTimeOffset.UtcNow,
+            LicenseEndDate = DateTimeOffset.UtcNow.AddMonths(1)
+        };
+        var hasher = new PasswordHasher<User>();
+        user.PasswordHash = hasher.HashPassword(user, "password123");
 
+        await _context.Users.AddAsync(user);
+        await _context.SaveChangesAsync();
+
+        var loginDto = new LoginDto
+        {
+            Email = "pepito@gmail.com",
+            Password = "password123"
+        };
 
         // Execution
-
+        var result = await _userService.LoginUserAsync(loginDto);
 
         // Assert
-        Assert
+        Assert.Null(result);
     }
 }
