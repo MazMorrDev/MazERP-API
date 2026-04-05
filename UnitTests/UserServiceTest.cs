263928df79b669c99c97e89ce2e77051dfa376b4
Marco Antonio Romero Albanez <122071151+MazMorrDev@users.noreply.github.com>
2026-04-04 21:52:06 -0400

    chore(UserServiceTest): correctly mock and configure builder

--- UnitTests/UserServiceTest.cs
+++ UnitTests/UserServiceTest.cs
@@ -2,25 +2,39 @@
 using MazErpAPI.Controllers;
 using MazErpAPI.Services.Implementation;
 using MazErpAPI.Services.Interfaces;
-using MazErpBack.Services.Implementation;
+using MazErpAPI.Utils.Mappers;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Moq;
 
 namespace UnitTests;
 
 public class UserServiceTest
 {
-
-    private readonly UserController _controller;
-    private readonly ILogger<IUserService> _logger;
-    private readonly IUserService _userService;
+    private readonly Mock<IConfiguration> _mockConfig;
+    private readonly Mock<ILogger<UserService>> _mockLogger;
+    private readonly Mock<ITokenService> _mockTokenService;
+    private readonly UserMapper _mapper;
     private readonly AppDbContext _context;
-    private readonly ITokenService _tokenService;
+    private readonly UserService _userService;
+    private readonly UserController _controller;
 
-    public UserServiceTest(){
-        _tokenService = new TokenService();
-        _userService = new UserService(_context, );
+    public UserServiceTest()
+    {
+        _mockConfig = new Mock<IConfiguration>();
+        _mockLogger = new Mock<ILogger<UserService>>();
+        _mockTokenService = new Mock<ITokenService>();
+        _mapper = new UserMapper();
+
+        // Configurar base de datos en memoria
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: "TestDb_" + Guid.NewGuid())
+            .Options;
+
+        _context = new AppDbContext(options);
+        _userService = new UserService(_context, _mockTokenService.Object, _mockLogger.Object, _mapper);
         _controller = new UserController(_userService);
-
     }
 
 
@@ -28,8 +42,8 @@ public class UserServiceTest
     public void LoginUser_WithInvalidPassword_ReturnsNull()
     {
         // Arrange
-        
-        
+
+
         // Execution
 
 
